7e763525e8905a9e7032dd47c4b9113670c3f789
Jakub Meyza <kubameyza@gmail.com>
2023-01-31 22:41:40 +0100

    Added functions to arrows

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let activeSlide = 1;
 let arrowLeft = document.querySelector(".arrow-left");
 let arrowRight = document.querySelector(".arrow-right");
 
@@ -16,21 +17,42 @@ let hideActiveSlide = () => {
 
 let showSlide = (slideNumber) => {
   hideActiveSlide();
-  document.querySelector("#slide" + slideNumber).classList.remove("active");
+  document.querySelector("#slide" + slideNumber).classList.add("active");
+};
+
+let showNextSlide = () => {
+  if (activeSlide == 3) {
+    activeSlide = 1;
+  } else {
+    activeSlide += 1;
+  }
+  showSlide(activeSlide);
+};
+
+let showPreviousSlide = () => {
+  if (activeSlide == 1) {
+    activeSlide = 3;
+  } else {
+    activeSlide -= 1;
+  }
+  showSlide(activeSlide);
 };
 
 let showSlide1 = () => {
   showSlide(1);
+  activeSlide = 1;
 };
-
 let showSlide2 = () => {
   showSlide(2);
+  activeSlide = 2;
 };
-
 let showSlide3 = () => {
   showSlide(3);
+  activeSlide = 3;
 };
 
 dot1.addEventListener("click", showSlide1);
 dot2.addEventListener("click", showSlide2);
 dot3.addEventListener("click", showSlide3);
+arrowRight.addEventListener("click", showNextSlide);
+arrowLeft.addEventListener("click", showPreviousSlide);
